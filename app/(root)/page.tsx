a8dbfdbb6f1f2fba3fc4f033c1677ad01d56b33a
Brian Shimkus <brianshimkus1@gmail.com>
2025-01-04 12:34:35 -0600

    Product list component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
+
 export default function Homepage() {
-	return <div>Page</div>
+	return (
+		<div>
+			<ProductList
+				data={sampleData.products}
+				title='Newest Arrivals'
+				limit={4}
+			/>
+		</div>
+	)
 }
