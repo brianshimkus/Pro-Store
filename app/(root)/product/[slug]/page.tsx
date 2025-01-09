06d578dee460a556c51269b7170c6eec9a482677
Brian Shimkus <brianshimkus1@gmail.com>
2025-01-08 23:45:13 -0600

    Product details - pull in product name

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import { getProductBySlug } from '@/lib/actions/product.actions'
+import { notFound } from 'next/navigation'
+
+export default async function ProductDetailsPage(props: {
+	params: Promise<{ slug: string }>
+}) {
+	const { slug } = await props.params
+	const product = await getProductBySlug(slug)
+
+	if (!product) notFound()
+
+	return <div>{product.name}</div>
+}
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,6 +3,7 @@
 import { PrismaClient } from '@prisma/client'
 import { convertToPlainObject } from '../utils'
 import { LATEST_PRODUCTS_LIMIT } from '../constants'
+import { prisma } from '@/db/prisma'
 
 // Get latest products
 export async function getLatestProducts() {
@@ -16,4 +17,13 @@ export async function getLatestProducts() {
 	})
 
 	return convertToPlainObject(data)
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+	return await prisma.product.findFirst({
+		where: {
+			slug,
+		},
+	})
 }
