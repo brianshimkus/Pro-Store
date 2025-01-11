4c7a719888426ee2c6d54906a150d02affcbb79e
Brian Shimkus <brianshimkus1@gmail.com>
2025-01-10 23:46:28 -0600

    Product Details Page

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import ProductPrice from '@/components/shared/product/product-price'
+import { Badge } from '@/components/ui/badge'
+import { Card, CardContent } from '@/components/ui/card'
 import { getProductBySlug } from '@/lib/actions/product.actions'
 import { notFound } from 'next/navigation'
 
@@ -9,5 +12,63 @@ export default async function ProductDetailsPage(props: {
 
 	if (!product) notFound()
 
-	return <div>{product.name}</div>
+	return (
+		<>
+			<section>
+				<div className='grid grid-cols-1 md:grid-cols-5'>
+					{/* Images Column */}
+					<div className='col-span-2'>{/* Product Images Component */}</div>
+					{/* Details Column */}
+					<div className='col-span-2 p-5'>
+						<div className='flex flex-col gap-6'>
+							<p>
+								{product.brand} {product.category}
+							</p>
+							<h1 className='h3-bold'>{product.name}</h1>
+							{/* Rating Component */}
+							<p>{product.numReviews} reviews</p>
+							<div className='flex flex-col sm:flex-row sm:items-center gap-3'>
+								<ProductPrice
+									value={Number(product.price)}
+									className='w-24 rounded-full bg-green-100 text-green-700 px-5 py-2'
+								/>
+							</div>
+						</div>
+						<div className='mt-10'>
+							<p className='font-semibold'>Description</p>
+							<p>{product.description}</p>
+						</div>
+					</div>
+					{/* Action Column */}
+					<div>
+						<Card>
+							<CardContent className='p-4'>
+								<div className='mb-2 flex justify-between'>
+									<div>Price</div>
+									<div>
+										<ProductPrice value={Number(product.price)} />
+									</div>
+								</div>
+								<div className='mb-2 flex justify-between'>
+									<div>Status</div>
+									{product.stock > 0 ? (
+										<Badge variant='outline'>In Stock</Badge>
+									) : (
+										<Badge variant='destructive'>Out Of Stock</Badge>
+									)}
+								</div>
+								{product.stock > 0 && (
+									<div className='flex-center'>{/* Add To Cart */}</div>
+								)}
+							</CardContent>
+						</Card>
+					</div>
+				</div>
+			</section>
+			<section className='mt-10'>
+				<h2 className='h2-bold mb-5'>Customer Reviews</h2>
+				{/* Review List */}
+			</section>
+		</>
+	)
 }
